f3206760496560cb7bd05421ab7227ae6c956dd5
zetahawke <michel.szinavel@gmail.com>
2020-03-06 09:47:05 -0300

    Feat: Adding some basic needed to store relation declarations and methods

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,2 +1,24 @@
 class Vehicle < ApplicationRecord
+  has_many :gps_waypoints
+
+  accepts_nested_attributes_for :gps_waypoints
+
+  class << self
+    def safe_params
+      %i[identifier]
+    end
+
+    def safe_nested_params
+      %i[identifier latitude longitude sent_at]
+    end
+
+    def save_waypoint(params)
+      vehicle = Vehicle.find_by(identifier: params[:vehicle_identifier])
+      if vehicle.present?
+        vehicle.gps_waypoints.create(params.slice(GpsWaypoint.safe_params))
+      else
+        vehicle.create!(params)
+      end
+    end
+  end
 end
