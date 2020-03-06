ed843a9dc64ae1b6c31ba349e2d60e1058095b32
zetahawke <michel.szinavel@gmail.com>
2020-03-06 12:48:41 -0300

    class instance shared among tests

--- spec/models/gps_waypoint_spec.rb
+++ spec/models/gps_waypoint_spec.rb
@@ -1,31 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe GpsWaypoint, :type => :model do
-  before do
-    vehicle = Vehicle.create(identifier: 'test')
-    @gps_waypoint = GpsWaypoint.new(
+  subject {
+    described_class.new(
       latitude: '-32.13221',
       longitude: '70.231231',
       sent_at: DateTime.current,
-      vehicle_id: vehicle.i
+      vehicle_id: Vehicle.create(identifier: 'test').id
     )
-  end
+  }
+
   it "is valid with valid attributes" do
-    expect(@gps_waypoint).to be_valid
+    expect(subject).to be_valid
   end
 
   it "is not valid without a latitude" do
-    @gps_waypoint.latitude = nil
-    expect(@gps_waypoint).to_not be_valid
+    subject.latitude = nil
+    expect(subject).to_not be_valid
   end
 
   it "is not valid without a longitude" do
-    @gps_waypoint.longitude = nil
-    expect(@gps_waypoint).to_not be_valid
+    subject.longitude = nil
+    expect(subject).to_not be_valid
   end
 
   it "is not valid without a sent_at" do
-    @gps_waypoint.sent_at = nil
-    expect(@gps_waypoint).to_not be_valid
+    subject.sent_at = nil
+    expect(subject).to_not be_valid
   end
 end
