a5158442fdfb28f9ea6b6bcbeed8bad06310fe00
zetahawke <michel.szinavel@gmail.com>
2020-03-06 15:23:35 -0300

    Adding gps_waypoints endpoint test

--- spec/models/gps_waypoint_spec.rb
+++ spec/models/gps_waypoint_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe GpsWaypoint, :type => :model do
+RSpec.describe GpsWaypoint, type: :model do
   subject {
     described_class.new(
       latitude: '-32.13221',
