f9a9e96919242f6d5c90c668b7c17dcd64057383
zetahawke <michel.szinavel@gmail.com>
2020-03-06 10:32:19 -0300

    Adding vehicle identifier on marker's click event

--- app/assets/javascripts/gps_waypoints.js
+++ app/assets/javascripts/gps_waypoints.js
@@ -1,9 +1,15 @@
 function loadVehiclesLastGpsWaypoints(vehicles, map) {
   vehicles.array.forEach(vehicle => {
-    new google.maps.Marker({
+    var marker = new google.maps.Marker({
       position: new google.maps.LatLng(vehicle.latitude, vehicle.longitude),
       map: map
     });
+    google.maps.event.addListener(marker, 'click', (function(marker) {
+      return function() {
+        infowindow.setContent(vehicle.identifier);
+        infowindow.open(map, marker);
+      }
+    })(marker));
   });
 }
 
