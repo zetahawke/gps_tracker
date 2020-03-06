3b29534cad323b132ca60a40afe17d580b199221
zetahawke <michel.szinavel@gmail.com>
2020-03-06 12:46:15 -0300

    Polishing basic test around models

--- spec/models/gps_waypoint_spec.rb
+++ spec/models/gps_waypoint_spec.rb
@@ -1,22 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe GpsWaypoint, :type => :model do
+  before do
+    vehicle = Vehicle.create(identifier: 'test')
+    @gps_waypoint = GpsWaypoint.new(
+      latitude: '-32.13221',
+      longitude: '70.231231',
+      sent_at: DateTime.current,
+      vehicle_id: vehicle.i
+    )
+  end
   it "is valid with valid attributes" do
-    expect(GpsWaypoint.new).to be_valid
+    expect(@gps_waypoint).to be_valid
   end
 
   it "is not valid without a latitude" do
-    gps_waypoint = GpsWaypoint.new(latitude: nil)
-    expect(gps_waypoint).to_not be_valid
+    @gps_waypoint.latitude = nil
+    expect(@gps_waypoint).to_not be_valid
   end
 
   it "is not valid without a longitude" do
-    gps_waypoint = GpsWaypoint.new(longitude: nil)
-    expect(gps_waypoint).to_not be_valid
+    @gps_waypoint.longitude = nil
+    expect(@gps_waypoint).to_not be_valid
   end
 
   it "is not valid without a sent_at" do
-    gps_waypoint = GpsWaypoint.new(sent_at: nil)
-    expect(gps_waypoint).to_not be_valid
+    @gps_waypoint.sent_at = nil
+    expect(@gps_waypoint).to_not be_valid
   end
 end
