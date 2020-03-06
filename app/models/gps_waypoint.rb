121514b28a6aacd20795f0c400a06e08d21c72be
zetahawke <michel.szinavel@gmail.com>
2020-03-06 09:47:05 -0300

    Feat: Adding some basic needed to store relation declarations and methods

--- app/models/gps_waypoint.rb
+++ app/models/gps_waypoint.rb
@@ -1,3 +1,11 @@
 class GpsWaypoint < ApplicationRecord
   belongs_to :vehicle
+
+  delegate :identifier, to: :vehicle, prefix: true
+
+  class << self
+    def safe_params
+      %i[latitude longitude sent_at vehicle_identifier]
+    end
+  end
 end
