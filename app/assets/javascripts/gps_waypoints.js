b1f899d68823eb7d7a674f46c6f9e4f5b6b2646e
zetahawke <michel.szinavel@gmail.com>
2020-03-09 09:53:46 -0300

    Fix: vehicles map location and description loaded through api

--- app/assets/javascripts/gps_waypoints.js
+++ app/assets/javascripts/gps_waypoints.js
@@ -1,23 +1,35 @@
-function loadVehiclesLastGpsWaypoints(vehicles, map) {
-  vehicles.array.forEach(vehicle => {
-    var marker = new google.maps.Marker({
+const loadVehicles = (map) => {
+  fetch('http://localhost:3000/gps_waypoints/last_vehicles_location')
+  .then(res => res.json())
+  .then((data) => {
+    showVehiclesLastGpsWaypoints(data.vehicles, map)
+  })
+  .catch((error) => {
+    console.log(error.message)
+  });
+};
+
+const showVehiclesLastGpsWaypoints = (vehicles, map) => {
+  const infowindow = new google.maps.InfoWindow();
+  vehicles.forEach(vehicle => {
+    const marker = new google.maps.Marker({
       position: new google.maps.LatLng(vehicle.latitude, vehicle.longitude),
       map: map
     });
-    google.maps.event.addListener(marker, 'click', (function(marker) {
-      return function() {
-        infowindow.setContent(vehicle.identifier);
+    google.maps.event.addListener(marker, 'click', ((marker) => {
+      return () => {
+        infowindow.setContent(vehicle.identifier + "\n" + vehicle.sent_at);
         infowindow.open(map, marker);
       }
     })(marker));
   });
-}
+};
 
-function initMap(vehicles) {
-  var mapOptions = {
+const initMap = () => {
+  const mapOptions = {
     zoom: 9,
     center: { lat: -33.445, lng: -70.669 },
   };
-  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-  loadVehiclesLastGpsWaypoints(vehicles, map);
-}
+  const map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  loadVehicles(map);
+};
--- app/controllers/gps_waypoints_controller.rb
+++ app/controllers/gps_waypoints_controller.rb
@@ -1,5 +1,10 @@
 class GpsWaypointsController < ApplicationController
-  def show
+  def show; end
+
+  def last_vehicles_location
     @vehicles = Vehicle.all.only_last_gps_waypoint
+    render json: { status: :ok, vehicles: @vehicles }, state: :ok
+  rescue StandardError => e
+    render json: { status: :error, message: e.message }, state: :bad_request
   end
 end
